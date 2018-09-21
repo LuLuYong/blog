a7945071bdd099d37a005fe38748bfa868192b33
wangxingkang <wangxingkang@sensoro.com>
2018-09-21 19:30:47 +0800

    chore: 添加依赖

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import { FastClick } from 'fastclick';
 import App from './App.vue';
 import router from './router/index';
 import store from './store/index';
 import './registerServiceWorker';
 
+FastClick.attach(document.body);
 Vue.config.productionTip = false;
 
 new Vue({
